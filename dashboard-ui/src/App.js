467848a67c3154c2bcf6a9d97a00a3176ab3e295
Brianlamm <hoanglamdieu@gmail.com>
2022-03-10 11:14:13 -0800

    Update App.js

--- dashboard-ui/src/App.js
+++ dashboard-ui/src/App.js
@@ -6,7 +6,7 @@ import AppStats from './components/AppStats'
 
 function App() {
 
-    const endpoints = ["ticket", "sale["]
+    const endpoints = ["/report/ticket", "/report/sale"]
 
     const rendered_endpoints = endpoints.map((endpoint) => {
         return <EndpointAudit key={endpoint} endpoint={endpoint}/>
